06a4a7b581af01178169f19f538feb6d419da8ed
ORJI FDAVOUR <Favour@titanname.com>
2024-05-03 21:38:35 -0700

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#puppet syntax to instal flask from pip3
-exec {'install flask':
-    command  => 'pip3 install flask==2.1.0',
-    path     => '/usr/bin/',
-    unless   => 'pip3 list | grep flask',
+#!/usr/bin/pup
+#puppet syntax to install flask
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
